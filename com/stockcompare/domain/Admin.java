156358cee8056408bb9eca61a7cf00bf96b1f7c7
1aktouf <aktoufi@roehampton.ac.uk>
2026-04-17 02:49:56 +0100

    Add Admin model

---
+++ com/stockcompare/domain/Admin.java
@@ -0,0 +1,26 @@
+package com.stockcompare.domain;
+
+public class Admin {
+
+    private String id;
+    private String name;
+    private String email;
+
+    public Admin(String id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
